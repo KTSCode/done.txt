4c6d03f789175eb0e0fb444b4467829749696686
Kyle San Clemente <kyle@oomba.com>
2017-05-11 01:02:37 -0700

    styled site and sized things

--- dist/daily-stand-up.php
+++ dist/daily-stand-up.php
@@ -3,16 +3,17 @@
   $pattern = "/x\s".$date."/";
   echo "<h2> Daily Stand Up For<br />".date('l, m-d-y', strtotime($date))."  </h2>";
   $handle = fopen("https://dl.dropboxusercontent.com/s/bpcy3u37uy0i6f7/done.txt?dl=0", "r");
-
+  echo "<ul>";
   if ($handle) {
     while (($line = fgets($handle)) !== false) {
       $task = preg_filter($pattern ,'', $line);
       if ($task && strpos($task, '@work') !== false) {
-        echo "<p>".$task."</p>";
+        echo "<li>".$task."</li>";
       }
     }
     fclose($handle);
   } else {
     echo "<h3 class=\"error\"> An error occured while trying to read done.txt </h3>";
   }
+  echo "</ul>";
 ?>
--- dist/index.php
+++ dist/index.php
@@ -1,11 +1,45 @@
 <html>
   <head>
     <title>done.txt</title>
+    <style>
+      body {
+        text-align: center;
+        font-size: 30px;
+        background-color: #111517;
+        color: #2ecc71;
+      }
+      h1 {
+        font-size: 64px;
+      }
+      h3 {
+        font-size: 44px;
+      }
+      li {
+        text-align: left;
+        margin-left: 5px;
+        font-size: 34px;
+      }
+      .button {
+        height: 100px;
+        width: 400px;
+        margin: 20px;
+        font-size: 40px;
+        background-color: #2ecc71;
+      }
+      #date-field {
+        width: 400px;
+        height: 100px;
+        font-size: 44px;
+      }
+      #dail-stand-up {
+        width: 400px;
+      }
+
+    </style>
   </head>
   <body style="display: flex; justify-content: center;">
     <div style="flex-direction: column; ">
       <?php echo '<h1>Done.txt</h1>'; ?>
-      <h3> Enter a date:</h3>
       <?php //This gets the last working day a
         $currentWeekDay = date( "w" );
         switch ($currentWeekDay) {
@@ -27,9 +61,9 @@
       <form id="form-date-form">
         <input type="date" id="date-field" value="<?php echo $lastWorkingDate; ?>" />
         <br />
-        <input type="submit" value="Get Tasks"/>
+        <input type="submit" value="Get Tasks" class="button"/>
       </form>
-      <div id="daily-stand-up">
+      <div id="daily-stand-up" style="width ">
       </div>
     </div>
     <script type="text/javascript" src="js/main.js"></script>
